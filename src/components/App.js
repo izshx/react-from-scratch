84413dd7606b04e6918bd41998bd08a7edd6f358
izephyx <maick_cs2@hotmail.com>
2019-12-28 01:18:21 -0300

    Definindo arquivos basicos da aplicaçao index e criando o component App

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>React from Scratch</h1>
+                Primeiro projeto em React ;)
+            </div>
+        );
+    }
+}
+
+export default App;
